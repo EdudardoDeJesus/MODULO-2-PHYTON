04a6df2f53709f61792a194d45aa75b56ec51ad1
Eduardo De Jesus <edy_586@hotmail.com>
2023-05-10 11:19:20 -0700

    Mis prácticas de Phyton

---
+++ print.py
@@ -0,0 +1,21 @@
+# Ejemplos de la función print
+
+# print("Hola mundo")
+# print("Hola mundo", "otra vez")
+# print("Son las", 9, "de la mañana")
+# print("El resultado de 3 * 4 es:", 3*4)
+
+#Ejemplos de cadenas formateadas
+# print("El número 15 en sistemas decinal es %d, en sistema octal es %o, en el sistema hexadecinal es %x" %(15, 15, 15))
+
+# pi = 3.141592
+# r = 5
+# print(f"El radio de un círculo es {r} y el área de ese círcilo es {pi * r ** 2 : .2f}")  
+
+# Impresión de carácterres especiales
+# print("La letra beta es: \n\t \u03B2")
+
+#Carácteres de escape
+print("Hola mundo", end = " ")
+print("otra vez", end = "\t ")
+print("y otra vez")
